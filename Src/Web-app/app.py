e8f6c24656e6f981944dbee99ba1e16d4eed5b71
lanxiang zhang <lanxiangzhang@lanxiangdeMBP.lan>
2023-12-15 13:59:30 +0200

    Update Webapp code, changed front-end layout and build the basic framework to load models.

--- Src/Web-app/app.py
+++ Src/Web-app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from model1 import predict1
+from model2 import predict2
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -6,6 +8,18 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict1', methods=['POST'])
+def predict_route1():
+    data = request.get_json()  # 获取前端发送的数据
+    prediction = predict1(data)  # 使用模型进行预测
+    return jsonify(prediction)  # 将预测结果返回到前端
+
+@app.route('/predict2', methods=['POST'])
+def predict_route2():
+    data = request.get_json()  # 获取前端发送的数据
+    prediction = predict2(data)  # 使用模型进行预测
+    return jsonify(prediction)  # 将预测结果返回到前端
 
 if __name__ == '__main__':
     app.run(debug=True)
